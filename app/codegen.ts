0d1a43950a157fca4920d6b8755fdda9d74798fc
Seyyyy <mysdc7@gmail.com>
2024-04-30 07:31:35 +0900

    GraphQLスキーマの配置変更

--- app/codegen.ts
+++ app/codegen.ts
@@ -2,8 +2,8 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "schema.graphql",
-  documents: "query.graphql",
+  schema: "src/app/graph/**/schema.graphql",
+  documents: "src/app/graph/**/document.graphql",
   generates: {
     "src/gql/client.ts": {
       plugins: [
