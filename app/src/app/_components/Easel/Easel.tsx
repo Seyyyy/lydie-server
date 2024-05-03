8d55de1855f5151640378236d4e45b321c59e4b3
Seyyyy <mysdc7@gmail.com>
2024-05-03 16:39:33 +0900

    Easelコンポーネントの作成

---
+++ app/src/app/_components/Easel/Easel.tsx
@@ -0,0 +1,39 @@
+import { ComponentProps } from "react";
+import Image from "next/image";
+import clsx from "clsx";
+
+type Props = {} & ComponentProps<"input">;
+
+export const Easel = (props: Props) => {
+  return (
+    <div className={clsx(`bg-bgBack rounded-lg relative`, props.className)}>
+      <div className="absolute w-full h-full flex justify-center">
+        {props.src ? (
+          <Image
+            // https://nextjs.org/docs/pages/api-reference/components/image#responsive-images
+            src={props.src}
+            alt={props.alt || "empty image"}
+            className="w-full h-full object-contain rounded-lg"
+            sizes="100vw"
+            style={{
+              width: "100%",
+              height: "auto",
+            }}
+            width={500}
+            height={500}
+          />
+        ) : (
+          <div className="m-4 w-[calc(100%-12px)] flex flex-col items-center justify-center border-2 border-dashed border-slate-400 rounded-lg">
+            <p className={`textButton text-gray-500`}>{`Please input file`}</p>
+          </div>
+        )}
+      </div>
+      <input
+        type="file"
+        accept="image/*"
+        {...props}
+        className="absolute w-full h-full opacity-0"
+      />
+    </div>
+  );
+};
