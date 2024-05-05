1b2eace0033a56c88daaeefe3cd0a7b908a2af96
Seyyyy <mysdc7@gmail.com>
2024-05-06 05:06:53 +0900

    テストベースのリンク追記

--- app/e2e/analyze/analyzeImage.spec.ts
+++ app/e2e/analyze/analyzeImage.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 import path from "path";
 
+// テストベース
+// https://github.com/Seyyyy/lydie-server/wiki/%E3%83%A6%E3%83%BC%E3%82%B9%E3%82%B1%E3%83%BC%E3%82%B9%E5%9B%B3#%E7%94%BB%E5%83%8F%E6%A4%9C%E8%A8%BC
 test.describe("画像分析機能の検証", () => {
   test("Entropyが出力されている", async ({ page }) => {
     await page.goto("http://localhost:3000/");
