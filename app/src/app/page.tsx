6d729cb6d6390bdc9d7946b3a55a683fe1ee9672
Seyyyy <20811739+Seyyyy@users.noreply.github.com>
2025-01-14 14:25:44 +0000

    feat: add create button

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { HomePage } from "./_static/HomePage";
 import { useImage } from "@/app/_models/image/useImage";
+import { useStore } from "@/app/_models/store/useStore";
 
 export default function Home() {
-  return <HomePage useImage={useImage} />;
+  return <HomePage useImage={useImage} useStore={useStore} />;
 }
